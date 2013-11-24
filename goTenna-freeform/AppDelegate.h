d371bfec9141319001528fff531353dac2e9996b
jyaunches <jmyaunch@gmail.com>
2013-11-24 18:53:29 -0500

    Initial Commit

---
+++ goTenna-freeform/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  goTenna-freeform
+//
+//  Created by Julietta Yaunches on 11/24/13.
+//  Copyright (c) 2013 Julietta Yaunches. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ goTenna-freeform/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  goTenna-freeform
+//
+//  Created by Julietta Yaunches on 11/24/13.
+//  Copyright (c) 2013 Julietta Yaunches. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
