146f5bdd405c0ab13f087246a3146779edcbcf5f
jyaunches <jmyaunch@gmail.com>
2013-11-25 17:24:07 -0500

    Adding gitignore and creating option to advertise a service using core bluetooth

--- goTenna-freeform/ViewController.h
+++ goTenna-freeform/ViewController.h
@@ -7,7 +7,8 @@
 //
 
 #import <UIKit/UIKit.h>
+#import <CoreBluetooth/CoreBluetooth.h>
 
-@interface ViewController : UIViewController
+@interface ViewController : UIViewController <CBPeripheralManagerDelegate>
 
 @end
--- goTenna-freeform/ViewController.m
+++ goTenna-freeform/ViewController.m
@@ -1,29 +1,60 @@
-//
-//  ViewController.m
-//  goTenna-freeform
-//
-//  Created by Julietta Yaunches on 11/24/13.
-//  Copyright (c) 2013 Julietta Yaunches. All rights reserved.
-//
-
 #import "ViewController.h"
+#import <CoreBluetooth/CoreBluetooth.h>
 
 @interface ViewController ()
-
+@property(nonatomic, strong) id myPeripheralManager;
 @end
 
 @implementation ViewController
+- (IBAction)advertisePeripheral:(id)sender {
+
+    CBUUID *myCharacteristicUUID = [CBUUID UUIDWithString:@"241858C4-6FBD-40DE-9C97-2EF41401EFA2"];
+    CBUUID *myServiceUUID = [CBUUID UUIDWithString:@"BEFFC411-C5C7-4569-8991-D3E4CD52231B"];
+
+    CBMutableCharacteristic *myCharacteristic =
+            [[CBMutableCharacteristic alloc] initWithType:myCharacteristicUUID
+                                               properties:CBCharacteristicPropertyRead
+                                                    value:nil permissions:CBAttributePermissionsReadable];
+
+    CBMutableService *myService = [[CBMutableService alloc] initWithType:myServiceUUID primary:YES];
+    myService.characteristics = @[myCharacteristic];
+
+    [self.myPeripheralManager addService:myService];
+
+    [self.myPeripheralManager startAdvertising:@{ CBAdvertisementDataServiceUUIDsKey :
+            @[myService.UUID] }];
+}
 
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
+	self.myPeripheralManager = [[CBPeripheralManager alloc] initWithDelegate:self queue:nil options:nil];
 }
 
 - (void)didReceiveMemoryWarning
 {
     [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+}
+
+- (void)peripheralManagerDidUpdateState:(CBPeripheralManager *)peripheral {
+
+}
+
+- (void)peripheralManager:(CBPeripheralManager *)peripheral
+            didAddService:(CBService *)service
+                    error:(NSError *)error {
+
+    if (error) {
+        NSLog(@"Error publishing service: %@", [error localizedDescription]);
+    }
+}
+
+- (void)peripheralManagerDidStartAdvertising:(CBPeripheralManager *)peripheral
+                                       error:(NSError *)error {
+
+    if (error) {
+        NSLog(@"Error advertising: %@", [error localizedDescription]);
+    }
 }
 
 @end
