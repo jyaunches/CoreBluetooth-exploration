c22cad0e9c87cd15bfd2f37a866aad74a7fcb337
jyaunches <jmyaunch@gmail.com>
2013-11-26 19:52:05 -0500

    Moving advertising to new view controller

---
+++ goTenna-freeform/AdvertiseAsPeripheralVC.h
@@ -0,0 +1,14 @@
+//
+//  AdvertiseAsPeripheralVC.h
+//  goTenna-freeform
+//
+//  Created by Julietta Yaunches on 11/26/13.
+//  Copyright (c) 2013 Julietta Yaunches. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AdvertiseAsPeripheralVC : UIViewController
+@property (weak, nonatomic) IBOutlet UILabel *statusLabel;
+
+@end
--- goTenna-freeform/AdvertiseAsPeripheralVC.m
+++ goTenna-freeform/AdvertiseAsPeripheralVC.m
@@ -1,19 +1,26 @@
-#import "ViewController.h"
+//
+//  AdvertiseAsPeripheralVC.m
+//  goTenna-freeform
+//
+//  Created by Julietta Yaunches on 11/26/13.
+//  Copyright (c) 2013 Julietta Yaunches. All rights reserved.
+//
+
+#import <CoreBluetooth/CoreBluetooth.h>
+#import "AdvertiseAsPeripheralVC.h"
 #import "AppDelegate.h"
 
-@interface ViewController ()
+@interface AdvertiseAsPeripheralVC ()
 @property(nonatomic, strong) CBPeripheralManager *myPeripheralManager;
 @end
 
-@implementation ViewController
+@implementation AdvertiseAsPeripheralVC
 
 - (void)viewDidLoad
 {
     [super viewDidLoad];
     self.myPeripheralManager = [[CBPeripheralManager alloc] initWithDelegate:self queue:nil options:nil];
-}
 
-- (IBAction)advertisePeripheral:(id)sender {
     CBUUID *myCharacteristicUUID = [CBUUID UUIDWithString:@"241858C4-6FBD-40DE-9C97-2EF41401EFA2"];
 
     CBMutableCharacteristic *myCharacteristic =
@@ -31,27 +38,26 @@
 }
 
 - (void)peripheralManagerDidUpdateState:(CBPeripheralManager *)peripheral {
-    NSLog(@"Peripheral manager updated state: %d", peripheral.state);
+    if(peripheral.state == CBPeripheralManagerStatePoweredOn){
+        self.statusLabel.text = @"Advertising Service";
+    }else{
+        self.statusLabel.text = [NSString stringWithFormat:@"Problem starting up. State: %d", peripheral.state];
+    }
 }
 
 - (void)peripheralManager:(CBPeripheralManager *)peripheral
             didAddService:(CBService *)service
                     error:(NSError *)error {
     if (error) {
-        NSLog(@"Error publishing service: %@", [error localizedDescription]);
+        self.statusLabel.text = [NSString stringWithFormat:@"Error publishing service: %@", [error localizedDescription];
     }
 }
 
 - (void)peripheralManagerDidStartAdvertising:(CBPeripheralManager *)peripheral
                                        error:(NSError *)error {
     if (error) {
-        NSLog(@"Error advertising: %@", [error localizedDescription]);
+        self.statusLabel.text = [NSString stringWithFormat:@"Error advertising: %@", [error localizedDescription];
     }
-}
-
-- (void)didReceiveMemoryWarning
-{
-    [super didReceiveMemoryWarning];
 }
 
 @end
